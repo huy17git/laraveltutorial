a1b3adb3df3b2d6f4a9c259cb0f29f042957c3ec
ng <sng198x@gmail.com>
2019-07-28 23:21:48 +0700

    create helpers

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', 'HomeController@index')->middleware('locale');
+Route::get('/menu', 'HomeController@menu');
 Route::get('/getStudentSubjects', 'StudentController@getSubjects');
 
 Route::get('/student/add', 'StudentController@create')->name('addStudent');
@@ -36,6 +37,3 @@ Route::post('/group/add', 'GroupController@store')->name('storeGroup');
 Route::get('group/ajaxStudent', 'GroupController@ajax')->name('ajaxGetStudent');
 
 Route::get('change-language/{language}', 'HomeController@changeLanguage')->name('change-language');
-
-
-
